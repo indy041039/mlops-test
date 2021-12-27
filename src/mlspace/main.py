5043ed37fb514a8daae3aa8350fe0fcedf8b8263
Ard-ong Suwanvipat <ardong_suwan@hotmail.com>
2021-12-27 16:15:59 +0700

    add workflow

--- src/mlspace/main.py
+++ src/mlspace/main.py
@@ -1,16 +1,24 @@
 import typer
 
-from mlspace.my_module import main_func
-from mlspace.my_module2 import add_one, minus_one, double_it, half_it
+from mlspace.math import add_one, minus_one, double_it, half_it
+from mlspace.string import main_func
 
 app = typer.Typer()
 
 @app.command()
-def say_something(message: str):
+def say_something(message: str = 'Hello'):
+    print(message)
     return message
 
 @app.command()
-def calculate_something(number: int):
+def calculate_something(number: int = 1):
     number = add_one(number)
     number = double_it(number)
+    print(number)
     return number
+
+@app.command()
+def required_args(message: str, number: int):
+    print(message)
+    print(number)
+    return message
